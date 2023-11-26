2b1eecca5e4f8e7129f7eda4d6605b3388bc3ecf
YevgeniyZubrin <zubrinevgeny@yandex.kz>
2023-11-26 20:52:16 +0600

    feat(sprint_1): all pages

---
+++ src/public/constants/index.js
@@ -0,0 +1,4 @@
+export { changePasswordFields } from './changePasswordFields'
+export { profileFields } from './profileFields'
+export { authorizationFields } from './authorizationFields'
+export { registrationFields } from './registrationFields'
