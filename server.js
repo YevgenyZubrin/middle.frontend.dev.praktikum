534fbf5e3d5961585d3e8d2b6d24270fbd5388cd
YevgeniyZubrin <zubrinevgeny@yandex.kz>
2024-04-28 12:09:58 +0500

    fix(sprint_2): express

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import express from 'express'
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 const app = express()
 
 app.use(express.static(resolve(__dirname, 'build')))
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000')
+})
